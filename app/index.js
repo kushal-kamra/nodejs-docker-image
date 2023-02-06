0e722d370cf61ac5f38f4f4b2b85942256b11f62
kushal-kamra <kushalkamradev@gmail.com>
2023-02-06 20:46:31 +0530

    added new docker command

--- app/index.js
+++ app/index.js
@@ -7,8 +7,11 @@ app.listen(9999, () => console.log("listening on 9999"));
 // To build
 // docker build -t nodeapp .
 
-// To run
+// To run with specific name and log in console
 // docker run --name nodeappsrvr1 -p 9999:9999 nodeapp
+
+// To just run the container image
+// docker run -d -p 8000:9999 nodeapp
 
 // To stop container
 // docker stop nodeappsrvr1
